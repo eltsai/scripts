df72b3508822d20d62a5da953ef13e61f8bbdb60
E-Tsai <etsai042@gmail.com>
2020-02-17 15:28:45 -0500

    add git repo checker

---
+++ check-git-repo-modifications.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+skip=(cooldown nda-plus-plus nCovMemory CPU-vulnerability-notes blog-source USTC_CG2020 naive-inits)
+dir=$(pwd)
+
+DGRAY='\033[0;30m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+cd ${HOME}/Projects
+for git_repo in $(ls)
+do
+    if [[ " ${skip[@]} " =~ " ${git_repo} " ]]; then
+        continue
+    fi
+    cd ${git_repo}
+    echo -e "${DGRAY}checking modifications for ${GREEN}" ${git_repo} "${NC}"
+    git status
+    echo ""
+    cd .. 
+done
+cd ${dir}
